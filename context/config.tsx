abc07df5e405d9199448fbad064667efec08feed
Benjythebee <Benjy.larcher@aol.com>
2025-12-24 14:53:22 +1300

    add default

--- context/config.tsx
+++ context/config.tsx
@@ -6,6 +6,27 @@ type StoreInteractiveData = Omit<InteractiveRow,'id'>
 
 const AppDataContext = createContext<StoreInteractiveData | null>(null);
 
+const defaultMetadata:StoreInteractiveData = {
+    image: '',
+    store_id: '',
+    created_at: new Date(),
+    metadata: {
+        trigger:{
+            type:'button',
+            position:{x:50,y:50},
+            label:'Click me',
+            scale:1,
+        },
+        action:{
+          type:'modal',
+            content:{
+                title:'Default Modal',
+                body:'This is a default modal content. Did you forget to set up your interactive data?'
+            }
+        },
+    },
+}
+
 export const InteractiveConfigProvider = ({ children }: { children: React.ReactNode }) => {
   const [data, setData] = useState<StoreInteractiveData | null>(null);
 
@@ -16,7 +37,10 @@ export const InteractiveConfigProvider = ({ children }: { children: React.ReactN
         console.log("Fetched interactive data:", d);
         setData(d);
       })
-      .catch(console.error);
+      .catch((err)=>{
+        console.error("Error fetching interactive data:", err);
+        setData(defaultMetadata)
+      });
   }, []);
 
   if (!data) return null; // or splash screen
